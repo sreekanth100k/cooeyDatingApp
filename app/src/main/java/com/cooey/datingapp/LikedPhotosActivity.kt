92b1c494f0ea5ec83de02464fd26681eb5816011
Sreekanth K <sreekanth100k@gmail.com>
2020-07-06 04:28:11 +0530

    Scrollbar now shows up in the screen

--- app/src/main/java/com/cooey/datingapp/LikedPhotosActivity.kt
+++ app/src/main/java/com/cooey/datingapp/LikedPhotosActivity.kt
@@ -3,10 +3,13 @@ package com.cooey.datingapp
 import android.app.Activity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSmoothScroller
+import androidx.recyclerview.widget.RecyclerView.SmoothScroller
 import com.cooey.datingapp.db.AppDb
 import com.cooey.datingapp.db.ProfileEntity
 import kotlinx.android.synthetic.main.layout_liked_photos_activity.*
-import java.util.ArrayList
+import java.util.*
+
 
 class LikedPhotosActivity:Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,7 @@ class LikedPhotosActivity:Activity() {
        var  profileList:List<ProfileEntity> = AppDb.getInMemoryDatabase(applicationContext).profileEntityMappingDAO().getProfilesWithStatus("true")
 
 
+
         var likedListAdapterObj:LikedListAdapter = LikedListAdapter(this,R.layout.card_layout,
             profileList as ArrayList<ProfileEntity>?
         )
@@ -24,9 +28,23 @@ class LikedPhotosActivity:Activity() {
         rv_liked_photos.setLayoutManager( LinearLayoutManager(this));
 
 
+
+
+
         rv_liked_photos.adapter = likedListAdapterObj
 
         (rv_liked_photos.adapter as LikedListAdapter).notifyDataSetChanged()
+
+//        val smoothScroller: SmoothScroller = object : LinearSmoothScroller(this) {
+//            override fun getVerticalSnapPreference(): Int {
+//                return SNAP_TO_START
+//            }
+//        }
+//
+//        smoothScroller.setTargetPosition(profileList.size);
+//        rv_liked_photos.layoutManager?.startSmoothScroll(smoothScroller);
+
+
 
     }
 
